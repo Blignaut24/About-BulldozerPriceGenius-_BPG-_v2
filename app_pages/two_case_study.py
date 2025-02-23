cc78bb5296db3ba946aa3b86074f65782cce5580
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2025-02-23 14:02:51 +0100

    feat: add data inspection toggle component to case study page

--- app_pages/two_case_study.py
+++ app_pages/two_case_study.py
@@ -1,5 +1,6 @@
 # Import the Streamlit library which we'll use to create our web app
 import streamlit as st
+import pandas as pd
 
 def case_study_body():
     # Add a main header for the overview section
@@ -9,3 +10,11 @@ def case_study_body():
     st.subheader(
         "BulldozerPriceGenius BPG: Know Your Equipment's Worth, Make Smarter Auction Decisions."
     )
+
+    # Load the CSV file
+    csv_file_path = "src/data_prep/TrainAndValid_object_values_as_categories.csv"
+    df = pd.read_csv(csv_file_path)
+
+    # Add a checkbox to toggle the display of the CSV table
+    if st.checkbox("Inspect dataframe"):
+        st.dataframe(df)
