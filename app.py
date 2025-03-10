4d2117958aa95f1bf4d4e8a9383fd012b947c597
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2025-03-10 06:09:19 +0100

    chore: update slugignore

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app_pages.multipage import MultiPage
 
 # Load pages scripts
 from app_pages.one_case_study import case_study_body
-from app_pages.two_case_study import case_study_body
+#from app_pages.two_case_study import case_study_body
 # from app_pages.three_model_performance import model_performance_body
 from app_pages.four_interactive_prediction import interactive_prediction_body
 from app_pages.five_documentation import documentation_body
@@ -13,7 +13,7 @@ app = MultiPage(app_name="BulldozerPriceGenius(BPG)")
 
 # App pages
 app.add_page("one_case_study", case_study_body)
-app.add_page("two_case_study", case_study_body)
+#app.add_page("two_case_study", case_study_body)
 # app.add_page("three_model_performance", model_performance_body)
 app.add_page("four_interactive_prediction", interactive_prediction_body)
 app.add_page("five_documentation", documentation_body)
